abd8bb9b98c4098285802188582922ddfc3a4590
Dimitrina Zlatkova <didi.zlatkova93@gmail.com>
2015-01-31 00:52:50 +0200

    routes scaffolding

---
+++ routes/routes.js
@@ -0,0 +1,62 @@
+module.exports = function(database) {
+	var users = database.collection('users');
+	var messages = database.collection('messages');
+
+	router.get('/', function(req, res) {
+		// tvyter homepage
+		// 1. if logged in user - show feed
+		// 	- profile preview : # of tvyts, followers, following
+		//	- popular tags
+		//	- last 20 tvyts of following users (endless scrolling), update in 20s
+		// 2. if new user - show login/register home page      
+	});
+
+	router.get('/feed', function(req, res) {
+		// tvyter feed
+		// 	- profile preview : # of tvyts, followers, following
+		//	- popular tags
+		//	- last 20 tvyts of following users (endless scrolling), update in 20s
+	});
+
+	router.get('/profile', function(req, res) {
+		// profile page + description
+		// show the profile of currently logged user
+		// 	- profile preview : # of tvyts, followers, following
+		//	- popular tags
+		//	- last 20 tvyts (endless scrolling)
+	});
+
+	router.get('/edit', function(req, res) {
+		// edit profile page
+		// show the profile of currently logged user 
+		// input fields for name, description and picture      
+	});
+
+	router.get('/followers', function(req, res) {
+		// profile previews for all followers + description + follow/unfollow link
+	});
+
+	router.get('/following', function(req, res) {
+		// profile previews for all following + description + follow/unfollow link
+	});
+
+	router.post('/login', function(req, res) {
+		// logs the user by username and password
+	});
+
+	router.post('/logout', function(req, res) {
+		// logs out the user
+	});
+
+	router.post('/register', function(req, res) {
+		// registers the user by name, username and password
+	});
+
+	router.post('/post', function(req, res) {
+		// posts a tvyt
+	});
+
+	router.post('/search', function(req, res) {
+		// searches tvyts by hashtags
+	});
+}
