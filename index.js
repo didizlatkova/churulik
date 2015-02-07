145b48f571e322035fff9e73cb458215c96aeaf3
Dimitrina Zlatkova <didi.zlatkova93@gmail.com>
2015-02-07 21:05:06 +0200

    fixed deprecated warnings

--- index.js
+++ index.js
@@ -9,15 +9,22 @@ var express = require('express'),
 	session = require('express-session');
 
 function setup_express(routes) {
-	app.use(session({ secret: 'nodejsisreallyawesome' })); // session secret
+	app.use(session({
+		secret: 'nodejsisreallyawesome',
+		resave: false,
+		saveUninitialized: false
+	}));
+
 	app.use(passport.initialize());
 	app.use(passport.session());
 	app.use(bodyParser.json());
-	app.use(bodyParser.urlencoded({ extended: false }));
+	app.use(bodyParser.urlencoded({
+		extended: false
+	}));
 	app.use('/public', express.static(path.join(__dirname, 'public')));
 	app.use('/img', express.static(path.join(__dirname, 'views/img')));
 	app.use('/', routes);
-	
+
 	// catch 404 and forward to error handler
 	app.use(function(req, res, next) {
 		var err = new Error('Not Found');
