375d858a43af40fd0281583361196377eb0fdb60
Dimitrina Zlatkova <didi.zlatkova93@gmail.com>
2015-02-09 00:11:33 +0200

    added Procfile.txt

--- public/events.js
+++ public/events.js
@@ -1,7 +1,7 @@
 $('#tvit-content').keyup(function(e) {
-	if ($('#tvit-button').hasClass('disabled') && this.value.length > 0) {
+	if ($('#tvit-button').hasClass('disabled') && this.value.length > 0 && this.value.length <= 140) {
 		$('#tvit-button').removeClass('disabled');
-	} else if (this.value.length === 0) {
+	} else if (this.value.length === 0 || this.value.length > 140) {
 		$('#tvit-button').addClass('disabled');
 	}
 });
