868a3b0bd3d6132d949a4aa29bac4297befbd27d
didizlatkova <didi.zlatkova93@gmail.com>
2015-05-19 15:39:03 +0300

    final touches on retweeting

--- public/events.js
+++ public/events.js
@@ -1,155 +1,157 @@
-(function () {
+(function() {
 
-var increaseMessageCount = function(){
-    var count = parseInt($('#messages-count').text(), 10) + 1;
-    $('#messages-count').text(count);
-    if (count === 1) {
-        $('#messages-text').text('твит');
-    } else {
-        $('#messages-text').text('твита');
-    }
-};
+    var increaseMessageCount = function() {
+        var count = parseInt($('#messages-count').text(), 10) + 1;
+        $('#messages-count').text(count);
+        if (count === 1) {
+            $('#messages-text').text('твит');
+        } else {
+            $('#messages-text').text('твита');
+        }
+    };
 
-$('#tvit-content').keyup(function() {
-    if ($('#tvit-btn').hasClass('disabled') && this.value.length > 0 && this.value.length <= 140) {
-        $('#tvit-btn').removeClass('disabled');
-    } else if (this.value.length === 0 || this.value.length > 140) {
+    $('#tvit-content').keyup(function() {
+        if ($('#tvit-btn').hasClass('disabled') && this.value.length > 0 && this.value.length <= 140) {
+            $('#tvit-btn').removeClass('disabled');
+        } else if (this.value.length === 0 || this.value.length > 140) {
+            $('#tvit-btn').addClass('disabled');
+        }
+    });
+
+    $(document).on('mouseover', '.message-wrapper', function() {
+        $(this).find('.delete-btn').show();
+        $(this).find('.retvit').show();
+    });
+
+    $(document).on('mouseleave', '.message-wrapper', function() {
+        $(this).find('.delete-btn').hide();
+        $(this).find('.retvit').hide();
+    });
+
+    $(document).on('click', '.delete-btn', function() {
+        if ($(this).hasClass('deleted')) {
+            return;
+        }
+
+        $(this).addClass('deleted');
+        var id = $(this).data("id");
+
+        $.post("/delete", {
+            id: id
+        }, function(data) {
+            $('#messages').html(data);
+        });
+
+        var count = parseInt($('#messages-count').text(), 10) - 1;
+        $('#messages-count').text(count);
+        if (count === 1) {
+            $('#messages-text').text('твит');
+        } else {
+            $('#messages-text').text('твита');
+        }
+    });
+
+    $(document).on('click', '#tvit-btn', function(e) {
+        e.preventDefault();
         $('#tvit-btn').addClass('disabled');
-    }
-});
+        var data = $('#add-tvit').serialize();
 
-$(document).on('mouseover', '.message-wrapper', function() {
-    $(this).find('.delete-btn').show();
-    $(this).find('.retvit').show();
-});
-
-$(document).on('mouseleave', '.message-wrapper', function() {
-    $(this).find('.delete-btn').hide();
-    $(this).find('.retvit').hide();
-});
-
-$(document).on('click', '.delete-btn', function() {
-    if ($(this).hasClass('deleted')) {
-        return;
-    }
-
-    $(this).addClass('deleted');
-    var id = $(this).data("id");
-
-    $.post("/delete", {
-        id: id
-    }, function(data) {
-        $('#messages').html(data);
-    });
-
-    var count = parseInt($('#messages-count').text(), 10) - 1;
-    $('#messages-count').text(count);
-    if (count === 1) {
-        $('#messages-text').text('твит');
-    } else {
-        $('#messages-text').text('твита');
-    }
-});
-
-$(document).on('click', '#tvit-btn', function(e) {
-    e.preventDefault();
-    $('#tvit-btn').addClass('disabled');
-    var data = $('#add-tvit').serialize();
-
-    $.post("/post", data, function(data) {
-        if (data) {
+        $.post("/post", data, function(data) {
             if (data) {
-                $('#messages').html(data);
+                if (data) {
+                    $('#messages').html(data);
+                }
             }
-        }
+        });
+
+        $('#add-tvit').trigger('reset');
+
+        increaseMessageCount();
     });
 
-    $('#add-tvit').trigger('reset');
+    $(document).on('click', '.follow-btn', function() {
+        $(this).addClass('disabled');
+        var user = $(this).data("user");
+        var loggedUser = $(this).data("logged");
 
-    increaseMessageCount();
-});
+        $.post("/follow", {
+            user: user
+        }, function(data) {
+            if (data) {
+                $('#follow-wrapper-' + user).html(data);
+            } else {
+                window.location.href = '/';
+            }
+        });
 
-$(document).on('click', '.follow-btn', function() {
-    $(this).addClass('disabled');
-    var user = $(this).data("user");
-    var loggedUser = $(this).data("logged");
-
-    $.post("/follow", {
-        user: user
-    }, function(data) {
-        if (data) {
-            $('#follow-wrapper-' + user).html(data);
+        var countFollowers = parseInt($('#followers-count-' + user).text(), 10) + 1;
+        $('#followers-count-' + user).text(countFollowers);
+        if (countFollowers === 1) {
+            $('#followers-text-' + user).text('следващ');
         } else {
-            window.location.href = '/';
+            $('#followers-text-' + user).text('следващи');
         }
-    });
 
-    var countFollowers = parseInt($('#followers-count-' + user).text(), 10) + 1;
-    $('#followers-count-' + user).text(countFollowers);
-    if (countFollowers === 1) {
-        $('#followers-text-' + user).text('следващ');
-    } else {
-        $('#followers-text-' + user).text('следващи');
-    }
-
-    var countFollowing = parseInt($('#following-count-' + loggedUser).text(), 10) + 1;
-    $('#following-count-' + loggedUser).text(countFollowing);
-    if (countFollowing === 1) {
-        $('#following-text-' + loggedUser).text('следван');
-    } else {
-        $('#following-text-' + loggedUser).text('следвани');
-    }
-});
-
-$(document).on('click', '.unfollow-btn', function() {
-    $(this).addClass('disabled');
-    var user = $(this).data("user");
-    var loggedUser = $(this).data("logged");
-
-    $.post("/unfollow", {
-        user: user
-    }, function(data) {
-        if (data) {
-            $('#follow-wrapper-' + user).html(data);
+        var countFollowing = parseInt($('#following-count-' + loggedUser).text(), 10) + 1;
+        $('#following-count-' + loggedUser).text(countFollowing);
+        if (countFollowing === 1) {
+            $('#following-text-' + loggedUser).text('следван');
         } else {
-            window.location.href = '/';
+            $('#following-text-' + loggedUser).text('следвани');
         }
     });
 
-    var countFollowers = parseInt($('#followers-count-' + user).text(), 10) - 1;
-    $('#followers-count-' + user).text(countFollowers);
-    if (countFollowers === 1) {
-        $('#followers-text-' + user).text('следващ');
-    } else {
-        $('#followers-text-' + user).text('следващи');
-    }
+    $(document).on('click', '.unfollow-btn', function() {
+        $(this).addClass('disabled');
+        var user = $(this).data("user");
+        var loggedUser = $(this).data("logged");
 
-    var countFollowing = parseInt($('#following-count-' + loggedUser).text(), 10) - 1;
-    $('#following-count-' + loggedUser).text(countFollowing);
-    if (countFollowing === 1) {
-        $('#following-text-' + loggedUser).text('следван');
-    } else {
-        $('#following-text-' + loggedUser).text('следвани');
-    }
-});
+        $.post("/unfollow", {
+            user: user
+        }, function(data) {
+            if (data) {
+                $('#follow-wrapper-' + user).html(data);
+            } else {
+                window.location.href = '/';
+            }
+        });
 
-$('#search-reset').click(function() {
-    $('#search-content').val('');
-    $('#messages').html('');
-});
+        var countFollowers = parseInt($('#followers-count-' + user).text(), 10) - 1;
+        $('#followers-count-' + user).text(countFollowers);
+        if (countFollowers === 1) {
+            $('#followers-text-' + user).text('следващ');
+        } else {
+            $('#followers-text-' + user).text('следващи');
+        }
 
-$(document).on('click', '.retvit', function() {
-    var user = $(this).data("user");
-    var content = $(this).data("content");
-    var location = $(this).data("location");
-    $.post("/post", {
-        retvitedFrom: user,
-        content: content,
-        location: location
-    }, function(data) {
-        $('#messages').html(data);
+        var countFollowing = parseInt($('#following-count-' + loggedUser).text(), 10) - 1;
+        $('#following-count-' + loggedUser).text(countFollowing);
+        if (countFollowing === 1) {
+            $('#following-text-' + loggedUser).text('следван');
+        } else {
+            $('#following-text-' + loggedUser).text('следвани');
+        }
     });
 
-    increaseMessageCount();
-});
+    $('#search-reset').click(function() {
+        $('#search-content').val('');
+        $('#messages').html('');
+    });
+
+    $(document).on('click', '.retvit', function() {
+        var retvitedFromUser = $(this).data("retvitedfrom");
+        var user = $(this).data("user");
+        var retvitedFrom = retvitedFromUser ? retvitedFromUser : user;
+        var content = $(this).data("content");
+        var location = $(this).data("location");
+        $.post("/post", {
+            retvitedFrom: retvitedFrom,
+            content: content,
+            location: location
+        }, function(data) {
+            $('#messages').html(data);
+        });
+
+        increaseMessageCount();
+    });
 })();
