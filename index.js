8e3790809169a8db0c86c2b1917d8d1557381ecb
Dimitrina Zlatkova <didi.zlatkova93@gmail.com>
2015-06-28 00:00:31 +0300

    fixed db connection

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ function setup_express(routes) {
     });
 }
 
-MongoClient.connect('mongodb://didizlatkova:Didi123@ds043981.mongolab.com:43981/tvityr', function(err, db) {
+MongoClient.connect(dburl, function(err, db) {
 	if (err) {
 		console.error('Cannot connect to the database', err);
 		return;
